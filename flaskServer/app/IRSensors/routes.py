e9b0356113f0c7095752b20732abc1ead8ad1fd3
guikfouri <guiokfouri@gmail.com>
2023-05-23 22:44:46 -0300

    Atualiza mascara e endpoints de turnOnOff

--- flaskServer/app/IRSensors/routes.py
+++ flaskServer/app/IRSensors/routes.py
@@ -1,42 +1,62 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app.main import bp
 from app.models.sensor import Sensor
 import requests
 
 
-url = "http://192.168.0.119"
+url = "http://192.168.0.123"
+
+COMMAND_ON = "on"
+COMMAND_OFF = "off"
 
 
 @bp.route("/turnOnOff/<int:sensor_id>/", methods=["GET", "POST"])
 def turnOnOff(sensor_id):
     sensor = Sensor.query.get(sensor_id)
     if request.method == "POST":
-        if request.form["turn_on"] == "on":
-            # requests.post(
-            #     url + "/turnOn", data={"command": "on", "macAddress": sensor.macAddress}
-            # )
-            return sensor.macAddress
-        elif request.form["turn_on"] == "off":
-            # requests.post(
-            #     url + "/turnOn",
-            #     data={"command": "off", "macAddress": sensor.macAddress},
-            # )
-            return render_template(
-                "IRSensors/turnOnOff.html", command="off", sensor_id=sensor_id
+        if request.form["turn_on"] == COMMAND_ON:
+            requests.post(
+                url + "/turnOn",
+                data={"command": COMMAND_ON, "macAddress": sensor.macAddress},
             )
+
+            return render_template(
+                "IRSensors/turnOnOff.html", command=COMMAND_ON, sensor_id=sensor_id
+            )
+
+        elif request.form["turn_on"] == COMMAND_OFF:
+            requests.post(
+                url + "/turnOn",
+                data={"command": COMMAND_OFF, "macAddress": sensor.macAddress},
+            )
+
+            return render_template(
+                "IRSensors/turnOnOff.html", command=COMMAND_OFF, sensor_id=sensor_id
+            )
+
         else:
-            return render_template("turnOnOff.html")
+            return redirect("/turnOnOff/{}".format(sensor_id))
+
     elif request.method == "GET":
         return render_template("IRSensors/turnOnOff.html", sensor_id=sensor_id)
 
 
 @bp.route("/turnOn/<int:sensor_id>/")
 def turnOn(sensor_id):
-    # requests.post(url + "/turnOn", data={"command": "on"})
-    return render_template("IRSensors/turnOnOff.html", sensor_id=sensor_id)
+    sensor = Sensor.query.get(sensor_id)
+    requests.post(
+        url + "/turnOn", data={"command": COMMAND_ON, "macAddress": sensor.macAddress}
+    )
+
+    return redirect("/turnOnOff/{}".format(sensor_id))
 
 
 @bp.route("/turnOff/<int:sensor_id>/")
 def turnOff(sensor_id):
-    # requests.post(url + "/turnOff", data={"command": "off"})
-    return render_template("IRSensors/turnOnOff.html", sensor_id=sensor_id)
+    sensor = Sensor.query.get(sensor_id)
+    requests.post(
+        url + "/turnOn",
+        data={"command": COMMAND_OFF, "macAddress": sensor.macAddress},
+    )
+
+    return redirect("/turnOnOff/{}".format(sensor_id))
